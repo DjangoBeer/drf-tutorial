c11db25c64668bcb7a057d93588bc7fb05cafd3f
Andrea Stagi <stagi.andrea@gmail.com>
2015-03-14 20:25:27 +0100

    Badge class based views

--- coffeecounter/api/urls.py
+++ coffeecounter/api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import badge_list, badge_detail
+from .views import BadgeDetail, BadgeList
 
 urlpatterns = [
-    url(r'^badges/$', badge_list),
-    url(r'^badges/(?P<pk>[0-9]+)$', badge_detail),
+    url(r'^badges/$', BadgeList.as_view()),
+    url(r'^badges/(?P<pk>[0-9]+)$', BadgeDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- coffeecounter/api/views.py
+++ coffeecounter/api/views.py
@@ -1,52 +1,60 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from .models import Badge
+from .serializers import BadgeSerializer
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.models import Badge
-from api.serializers import BadgeSerializer
+from rest_framework import status
 
 
-@api_view(['GET', 'POST'])
-def badge_list(request):
+class BadgeList(APIView):
     """
     List all badges or create a new badge.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         badges = Badge.objects.all()
         serializer = BadgeSerializer(badges, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = BadgeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def badge_detail(request, pk):
-    """
-    Retrieve, update or delete a badge.
-    """
-    try:
-        badge = Badge.objects.get(pk=pk)
-    except Badge.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class BadgeDetail(APIView):
+    """
+    Retrieve, update or delete a badge instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Badge.objects.get(pk=pk)
+        except Badge.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        badge = self.get_object(pk)
         serializer = BadgeSerializer(badge)
         return Response(serializer.data)
 
-    elif request.method == 'PUT' or request.method == 'PATCH':
-        partial_update = False
-        serializer = BadgeSerializer(
-            badge, data=request.data,
-            partial=(request.method == 'PATCH')
-        )
+    def put(self, request, pk, format=None):
+        badge = self.get_object(pk)
+        serializer = BadgeSerializer(badge, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def patch(self, request, pk, format=None):
+        badge = self.get_object(pk)
+        serializer = BadgeSerializer(badge, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        badge = self.get_object(pk)
         badge.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
