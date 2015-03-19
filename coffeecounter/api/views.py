3af7862d5652de0217c65f405760ea6c87e0940a
Andrea Stagi <stagi.andrea@gmail.com>
2015-03-19 08:21:47 +0100

    Refactoring using generic class based views

--- coffeecounter/api/views.py
+++ coffeecounter/api/views.py
@@ -2,13 +2,10 @@ from .models import Badge, Consumption
 from .serializers import BadgeSerializer, ConsumptionSerializer
 from .permissions import IsSuperUserOrReadOnly
 from rest_framework import permissions
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class BadgeList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+class BadgeList(generics.ListCreateAPIView):
     """
     List all badges or create a new badge.
     """
@@ -16,69 +13,30 @@ class BadgeList(mixins.ListModelMixin,
     serializer_class = BadgeSerializer
     permission_classes = (IsSuperUserOrReadOnly,)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class BadgeDetail(mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.DestroyModelMixin,
-                  generics.GenericAPIView):
+class BadgeDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a badge instance.
-
-    If you're writing your own views and want to enforce object level permissions,
-    or if you override the get_object method on a generic view, then you'll need to
-    explicitly call the .check_object_permissions(request, obj) method on the view
-    at the point at which you've retrieved the object.
     """
     queryset = Badge.objects.all()
     serializer_class = BadgeSerializer
     permission_classes = (IsSuperUserOrReadOnly,)
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class ConsumptionList(mixins.ListModelMixin,
-                      mixins.CreateModelMixin,
-                      generics.GenericAPIView):
+class ConsumptionList(generics.ListAPIView):
     """
     List all consumptions.
     """
     queryset = Consumption.objects.all()
     serializer_class = ConsumptionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-
-class ConsumptionListPerUser(mixins.ListModelMixin,
-                             mixins.CreateModelMixin,
-                             generics.GenericAPIView):
+class ConsumptionListPerUser(generics.ListCreateAPIView):
     """
     List all the auth users consumptions or create a new consumption.
     """
     serializer_class = ConsumptionSerializer
     permission_classes = (permissions.IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
