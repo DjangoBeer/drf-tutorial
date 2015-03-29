9ffac68b0280d5c5dfda1890c94dbcf2747dfa53
Andrea Stagi <stagi.andrea@gmail.com>
2015-03-29 22:31:13 +0200

    Add IsOwnerOrReadOnly custom permission

--- coffeecounter/api/permissions.py
+++ coffeecounter/api/permissions.py
@@ -23,3 +23,18 @@ class IsSuperUserOrReadOnly(permissions.BasePermission):
 
         # You need to be superuser to do POST, PUT and PATCH requests.
         return request.user.is_superuser
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the object.
+        return obj.user == request.user
